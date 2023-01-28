b35c7df5012ef6db98fd10b1a241cc8908339024
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2023-01-28 20:41:08 +0100

    chore: build

--- dist/index.js
+++ dist/index.js
@@ -1,42 +1,6 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 4874:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
-
-"use strict";
-
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.isPost = void 0;
-const core = __importStar(__nccwpck_require__(2186));
-exports.isPost = !!core.getState('isPost');
-
-
-/***/ }),
-
 /***/ 3109:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -78,13 +42,13 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(2186));
 const github = __importStar(__nccwpck_require__(5438));
 const fs = __importStar(__nccwpck_require__(3292));
-const state_1 = __nccwpck_require__(4874);
 function main() {
     var _a;
     return __awaiter(this, void 0, void 0, function* () {
+        core.info("Starting Label Sync");
         try {
             // TODO: Make this path configurable
-            const path = "./github/issues.json";
+            const path = "./github/labels.json";
             const token = core.getInput('token', { required: true });
             const content = yield fs.readFile(path, "utf8");
             const localLabels = JSON.parse(content);
@@ -103,9 +67,7 @@ function main() {
         }
     });
 }
-if (state_1.isPost) {
-    main();
-}
+main();
 
 
 /***/ }),
